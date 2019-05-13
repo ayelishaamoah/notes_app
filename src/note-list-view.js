380c8a0074c00d5e6eef335d2b7514632504f160
Ayelisha Amoah <ayelisha.amoah@gmail.com>
2019-05-13 18:11:49 +0100

    Add renderHtml method to NoteListView

--- src/note-list-view.js
+++ src/note-list-view.js
@@ -2,5 +2,9 @@
   function NoteListView() {
     this.noteList = new NoteList
   };
+
+  NoteListView.prototype.renderHtml = function() {
+    return `<ul><li><div>${this.noteList.notes[0].text }</div></li>`
+  };
   exports.NoteListView = NoteListView
 })(this);
